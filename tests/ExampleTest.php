2446f19aa09e5ae7546251c45edec2814f129405
romapost <roman.postovalov@gmail.com>
2020-11-23 21:23:50 +0100

    Added phpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TestUnit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testOk()
+    {
+        $this->assertTrue(false);
+    }
+}
